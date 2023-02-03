a5d679ac3cab69a1d69f878e7cb9a9768405d255
rucin93 <rucin93@gmail.com>
2023-02-03 22:43:10 +0100

    Fix indent

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,9 @@ use bloom::{BloomFilter, ASMS};
 use tokio::task;
 
 const DB: &str = "Bitcoin_addresses_LATEST.txt";
-// const ERROR_RATE: f32 = 0.0001;
 const ERROR_RATE: f32 = 0.00001;
 const EXPECTED_SIZE: u32 = 50_000_000; // more than 40kk to make sure that filter is not exceeded
 const DEBUG: bool = false;
-// const MAX_CORES: usize = 1;
 const TEST: &str = "bc1q225sldtdwtjt7rm8ugedpurzujwdkt3c2xv4pt";
 
 #[tokio::main]
@@ -51,22 +49,13 @@ async fn main() {
     println!( "Load database completed in {:.2?} - size: {:?}", timer.elapsed(), database.len() );
 
     // test
-
     if filter.contains(&TEST) && database.contains(&TEST.clone().to_string()) {
         println!("Address is inside - Test passed")
     }
-    // single thread
-    // worker(&database, &filter);
 
-    // Multithread
     let database_ = Arc::new(RwLock::new(database));
     let filter_ = Arc::new(RwLock::new(filter));
-
-
     let num_cores = num_cpus::get();
-    // if MAX_CORES > 0 && false {
-        // num_cores = MAX_CORES;
-    // }
 
     println!("Running on {} cores", num_cores);
     
@@ -85,17 +74,17 @@ async fn main() {
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+  where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
 fn write_to_file(data: &str, file_name: &str) {
-    let mut file = OpenOptions::new()
-        .append(true)
-        .open(file_name)
-        .expect("Unable to open file");
-    file.write_all(data.as_bytes()).unwrap();
+  let mut file = OpenOptions::new()
+    .append(true)
+    .open(file_name)
+    .expect("Unable to open file");
+  file.write_all(data.as_bytes()).unwrap();
 }
 
 fn check_address(
@@ -108,49 +97,49 @@ fn check_address(
     let address_string = address.to_string();
     if database.contains(&address_string) {
         let data = format!(
-            "{}{}{}{}{}{}{}{}{}",
-            secret_key.display_secret(),
-            "\n",
-            private_key.to_wif(),
-            "\n",
-            public_key.to_string(),
-            "\n",
-            address_string.as_str(),
-            "\n",
-            "\n",
+          "{}{}{}{}{}{}{}{}{}",
+          secret_key.display_secret(),
+          "\n",
+          private_key.to_wif(),
+          "\n",
+          public_key.to_string(),
+          "\n",
+          address_string.as_str(),
+          "\n",
+          "\n",
         );
         write_to_file(data.as_str(), found_file_path("found.txt".to_string()).as_str());
-        println!("{}", data.as_str())
+        println!("FOUND: {}", data.as_str())
     }
 }
 
 fn found_file_path(file: String) -> String {
-    let mut path = std::env::current_dir().unwrap();
-    path.push(file);
-    path.to_str().unwrap().to_string()
+  let mut path = std::env::current_dir().unwrap();
+  path.push(file);
+  path.to_str().unwrap().to_string()
 }
 
 fn worker(database: &Vec<String>, filter: &BloomFilter) {
-    let mut count: f64 = 0.0;
-    let start = Instant::now();
-    loop {
-        let secp = Secp256k1::new();
-        let secret_key = SecretKey::new(&mut rand::thread_rng());
-        let private_key = PrivateKey::new(secret_key, Network::Bitcoin);
-        let public_key = key::PublicKey::from_private_key(&secp, &private_key);
-        let address = Address::p2pkh(&public_key, Network::Bitcoin);
+  let mut count: f64 = 0.0;
+  let start = Instant::now();
+  loop {
+    let secp = Secp256k1::new();
+    let secret_key = SecretKey::new(&mut rand::thread_rng());
+    let private_key = PrivateKey::new(secret_key, Network::Bitcoin);
+    let public_key = key::PublicKey::from_private_key(&secp, &private_key);
+    let address = Address::p2pkh(&public_key, Network::Bitcoin);
 
-        if filter.contains(&address) {
-            check_address(&private_key, secret_key, &address, database, public_key);
-        }
-
-        if DEBUG {
-            count += 1.0;
-            if count % 10000.0 == 0.0 {
-                let current_core = std::thread::current().id();
-                let elapsed = start.elapsed().as_secs_f64();
-                println!( "Core {:?} hash/s: {} last address: {}", current_core, (count / elapsed).round(), &address );
-            }
-        }
+    if filter.contains(&address) {
+      check_address(&private_key, secret_key, &address, database, public_key);
     }
+
+    if DEBUG {
+      count += 1.0;
+      if count % 10000.0 == 0.0 {
+        let current_core = std::thread::current().id();
+        let elapsed = start.elapsed().as_secs_f64();
+        println!( "Core {:?} hash/s: {} last address: {}", current_core, (count / elapsed).round(), &address );
+      }
+    }
+  }
 }
