3d17d8bbaafffb92edccbd03550da66ce37ae738
rucin93 <rucin93@gmail.com>
2023-02-05 18:36:27 +0100

    Minor tweaks

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate secp256k1;
 extern crate num_cpus;
 extern crate bloom;
 
+// use sha3::{Digest, Keccak256};
+
 use std::collections::HashSet;
 use std::fs::{OpenOptions};
 use std::io::{self, BufRead};
@@ -35,12 +37,12 @@ async fn main() {
 
     let mut database1 = HashSet::new();
     let mut database3 = HashSet::new();
-    let mut databaseB = HashSet::new();
+    let mut database_b = HashSet::new();
     let timer = Instant::now();
 
     let mut filter1: BloomFilter = BloomFilter::with_rate(ERROR_RATE, EXPECTED_SIZE / 3);
     let mut filter3: BloomFilter = BloomFilter::with_rate(ERROR_RATE, EXPECTED_SIZE / 3);
-    let mut filterB: BloomFilter = BloomFilter::with_rate(ERROR_RATE, EXPECTED_SIZE / 3);
+    let mut filter_b: BloomFilter = BloomFilter::with_rate(ERROR_RATE, EXPECTED_SIZE / 3);
 
     if let Ok(lines) = read_lines(DB) {
         for line in lines {
@@ -52,8 +54,8 @@ async fn main() {
                     database3.insert(adress.to_string());
                     filter3.insert(&adress);
                 } else if adress.to_string().chars().nth(0) == Some('b') {
-                    databaseB.insert(adress.to_string());
-                    filterB.insert(&adress);
+                    database_b.insert(adress.to_string());
+                    filter_b.insert(&adress);
                 }
             }
         }
@@ -61,36 +63,36 @@ async fn main() {
 
     println!( "Load database completed in {:.2?} ", timer.elapsed() );
 
-    if filterB.contains(&TEST) && databaseB.contains(&TEST.clone().to_string()) {
+    if filter_b.contains(&TEST) && database_b.contains(&TEST.clone().to_string()) {
         println!("Address is inside - Test passed")
     }
 
     // Multithread
     let database1_ = Arc::new(RwLock::new(database1));
     let database3_ = Arc::new(RwLock::new(database3));
-    let databaseB_ = Arc::new(RwLock::new(databaseB));
+    let database_b = Arc::new(RwLock::new(database_b));
     let filter1_ = Arc::new(RwLock::new(filter1));
     let filter3_ = Arc::new(RwLock::new(filter3));
-    let filterB_ = Arc::new(RwLock::new(filterB));
+    let filter_b = Arc::new(RwLock::new(filter_b));
     
     for th in 0..num_cpus::get() {
         let clone_database1_ = Arc::clone(&database1_);
         let clone_database3_ = Arc::clone(&database3_);
-        let clone_databaseB_ = Arc::clone(&databaseB_);
+        let clone_database_b = Arc::clone(&database_b);
         let clone_filter1_ = Arc::clone(&filter1_);
         let clone_filter3_ = Arc::clone(&filter3_);
-        let clone_filterB_ = Arc::clone(&filterB_);
+        let clone_filter_b = Arc::clone(&filter_b);
       
         task::spawn_blocking(move || {
             println!("Core {:?} started", th);
             let db1 = clone_database1_.read().unwrap();
             let db3 = clone_database3_.read().unwrap();
-            let dbB = clone_databaseB_.read().unwrap();
+            let db_b = clone_database_b.read().unwrap();
             let f1 = clone_filter1_.read().unwrap();
             let f3 = clone_filter3_.read().unwrap();
-            let fB = clone_filterB_.read().unwrap();
+            let f_b = clone_filter_b.read().unwrap();
             
-            worker(&db1, &db3, &dbB, &f1, &f3, &fB, &th);
+            worker(&db1, &db3, &db_b, &f1, &f3, &f_b, &th);
         });
     }
 }
@@ -142,7 +144,7 @@ fn found_file_path(file: String) -> String {
     path.to_str().unwrap().to_string()
 }
 
-fn worker(database1: &HashSet<String>, database3: &HashSet<String>, databaseB: &HashSet<String>, filter1: &BloomFilter, filter3: &BloomFilter, filterB: &BloomFilter, th: &usize) {
+fn worker(database1: &HashSet<String>, database3: &HashSet<String>, database_b: &HashSet<String>, filter1: &BloomFilter, filter3: &BloomFilter, filter_b: &BloomFilter, th: &usize) {
     let start = Instant::now();
     let mut count: f64 = 0.0;
 
@@ -151,27 +153,35 @@ fn worker(database1: &HashSet<String>, database3: &HashSet<String>, databaseB: &
         let secret_key = SecretKey::new(&mut rand::thread_rng());
         let private_key = PrivateKey::new(secret_key, Network::Bitcoin);
         let public_key = key::PublicKey::from_private_key(&secp, &private_key);
-        let addressP2pkh = Address::p2pkh(&public_key, Network::Bitcoin);
-        let addressP2wpkh = Address::p2wpkh(&public_key, Network::Bitcoin).unwrap();
-        let addressP2shwpkh = Address::p2shwpkh(&public_key, Network::Bitcoin).unwrap();
+        let address_p2pkh = Address::p2pkh(&public_key, Network::Bitcoin);
+        let address_p2wpkh = Address::p2wpkh(&public_key, Network::Bitcoin).unwrap();
+        let address_p2shwpkh = Address::p2shwpkh(&public_key, Network::Bitcoin).unwrap();
+        // let address_eth = Keccak256::digest(&public_key.to_bytes()).as_slice()[12..].to_vec();
 
-        if filter1.contains(&addressP2pkh) {
-            check_address(&private_key, secret_key, &addressP2pkh, database1, public_key);
+        // address_eth - convert to hex
+        // let mut address_eth_hex = "0x".to_string();
+        // for byte in address_eth {
+        //     address_eth_hex.push_str(&format!("{:02x}", byte));
+        // }
+        // println!("ETH: {}", address_eth_hex);
+
+        if filter1.contains(&address_p2pkh) {
+            check_address(&private_key, secret_key, &address_p2pkh, database1, public_key);
         }
 
-        if filterB.contains(&addressP2wpkh) {
-            check_address(&private_key, secret_key, &addressP2wpkh, databaseB, public_key);
+        if filter_b.contains(&address_p2wpkh) {
+            check_address(&private_key, secret_key, &address_p2wpkh, database_b, public_key);
         }
 
-        if filter3.contains(&addressP2wpkh) {
-            check_address(&private_key, secret_key, &addressP2wpkh, database3, public_key);
+        if filter3.contains(&address_p2wpkh) {
+            check_address(&private_key, secret_key, &address_p2wpkh, database3, public_key);
         }
 
         if DEBUG {
             count += 3.0;
-            if count % 100000.0 == 0.0 {
+            if count % 10000.0 == 0.0 {
                 println!( "Core {:?} hash/s: {} last address: {} {} {}",
-                th, (count / start.elapsed().as_secs_f64()).round(), &addressP2pkh, &addressP2wpkh, &addressP2shwpkh );
+                th, (count / start.elapsed().as_secs_f64()).round(), &address_p2pkh, &address_p2wpkh, &address_p2shwpkh );
             }
         }
     }
